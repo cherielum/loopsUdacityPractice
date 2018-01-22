ca43013e403697cfef3023210c3435802a46303c
Cherie Lum <charismatic.cherie@gmail.com>
2018-01-21 20:47:49 -0500

    adding fizz buzz

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
 
 var x = 1; 
 while(x<= 10000) {
-    console.log(x + "mississippi!");
+    console.log(x +" mississippi!");
     x = x+1;
+}
+
+// Fizbuzz variation 
+// Write a while loop that:
+
+// Loop through the numbers 1 to 20
+// If the number is divisible by 3, print "Julia"
+// If the number is divisible by 5, print "James"
+// If the number is divisible by 3 and 5, print "JuliaJames"
+// If the number is not divisible by 3 or 5, print the number
+
+
+var x = 1;
+
+while (x<= 20) {
+    if ( (x%5 ===0)  && (x%3 ===0) ){
+        console.log("JuliaJames");
+    } else if (x % 3 === 0){
+        console.log("Julia");
+    }
+    else if (x % 5 ===0 ){
+        console.log("James");
+    }
+    else {
+        console.log(x);
+    }
+    x++; 
 }
